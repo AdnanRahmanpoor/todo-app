d1ab19dd63ce030c60b5e5b81f98ac2130148651
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-24 13:50:26 +0400

    Completed TodoItem.tsx code & edits to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import AddTodo from './components/AddTodo';
 import TodoItem from './components/TodoItem';
 import TodoList from './components/TodoList';
+import Todo from './Todo';
 
-const App = () => {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   return (
     <>
       <div>App</div>
       <TodoList />
-      <TodoItem />
+      <TodoItem todo={todos}  />
       <AddTodo />
     </>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Todo from '../Todo';
 
-const TodoItem = () => {
-  return (
-    <div>TodoItem</div>
-  )
+interface TodoItemProps {
+  todo: Todo;
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
 }
 
-export default TodoItem
+const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  toggleTodo,
+  deleteTodo,
+}) => {
+  return (
+    <div>
+      <input
+        type='checkbox'
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default TodoItem;
